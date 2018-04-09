bde2b845480598d86a103cfee9ec9fd8e9a40524
Wei-Ting Tu <weiting@maxeda-tech.com>
2018-04-10 01:51:26 +0800

    Add Foo test

---
+++ input/Foo1.java
@@ -0,0 +1,21 @@
+class FooTest
+{
+    public static void main( String[] a )
+    {
+        System.out.println( new Foo().Bar() );
+    }
+}
+// This class contains an array of integers and
+// methods to initialize, print and search the array
+// using Binary Search
+
+class Foo
+{
+    int size ;
+
+    public int Bar()
+    {
+        Foo = 1;
+        return 0;
+    }
+}
